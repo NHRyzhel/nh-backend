bc609a3fc3480ef0926e3f5bc3374fc3d333a13e
Ryzhel <ryz.game.id@gmail.com>
2025-07-24 13:54:52 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -68,5 +68,8 @@ def optimize():
         'stat': best_stat
     })
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
